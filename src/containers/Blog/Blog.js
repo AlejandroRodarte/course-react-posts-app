bee77dd1913801d8e3b17af869d281d7b5e7a2d3
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-06 13:42:18 -0600

    Adding Interceptors to Execute Code Globally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -16,7 +16,7 @@ class Blog extends Component {
 
     componentDidMount() {
         axios
-            .get('https://jsonplaceholder.typicode.com/pos ts')
+            .get('https://jsonplaceholder.typicode.com/posts')
             .then(({ data: posts }) => this.setState(() => ({ posts: posts.slice(0, 4).map(post => ({ ...post, author: 'Alex' })) })))
             .catch(() => this.setState(() => ({ error: true })));
     }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+axios.interceptors.request.use((req) => {
+    console.log(req);
+    return req;
+}, (err) => {
+    console.log(err);
+    return Promise.reject(err);
+});
+
+axios.interceptors.response.use((res) => {
+    console.log(res);
+    return res;
+}, (err) => {
+    console.log(err);
+    return Promise.reject(err);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
