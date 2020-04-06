5f6ab80789c4d314e0bb2cc455bfa50c1fc2ada3
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-06 13:32:09 -0600

    Sending a DELETE Request

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -23,6 +23,11 @@ class FullPost extends Component {
 
     }
 
+    deletePostHandler = () => 
+        axios
+            .delete(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+            .then(console.log);
+
     render () {
         
         const noPostStyles = {
@@ -42,7 +47,12 @@ class FullPost extends Component {
                     <h1>{ this.state.post.title }</h1>
                     <p>{ this.state.post.body }</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button 
+                            className="Delete"
+                            onClick={ this.deletePostHandler }
+                        >
+                            Delete
+                        </button>
                     </div>
                 </div>
             );
